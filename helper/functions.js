6fe14eb590e24b11030dcee476c732abc3817ee3
EStanys <edvinasstanys@gmail.com>
2021-05-28 15:26:58 +0300

    backend done

--- helper/functions.js
+++ helper/functions.js
@@ -1,10 +1,34 @@
 const todoDb = require('../db/todoDb');
+let todoId = 3;
 
 function findTodoWithId(id) {
   console.log('findTodoWithId ran');
   return todoDb.find((t) => t.id === +id);
 }
 
+function handleFindErr(paramId, res) {
+  console.log('todo not found');
+  return res.status(404).json({ error: `todo with id: ${paramId} does not exist` });
+}
+
+function handleDelete(found) {
+  // todel kad todoDb yra const, mes rasim norimo istrinti index
+  // ir pasalinsim is masyvo
+  const index = todoDb.indexOf(found);
+  todoDb.splice(index, 1);
+}
+function handleAdd(title) {
+  const newTodo = {
+    id: ++todoId,
+    title: title,
+    done: false,
+  };
+  todoDb.push(newTodo);
+}
+
 module.exports = {
   findTodoWithId,
+  handleFindErr,
+  handleDelete,
+  handleAdd,
 };
--- index.js
+++ index.js
@@ -1,17 +1,70 @@
 const express = require('express');
 
-const { findTodoWithId } = require('./helper/functions');
+const { findTodoWithId, handleFindErr, handleDelete, handleAdd } = require('./helper/functions');
 const todoDb = require('./db/todoDb');
 const app = express();
 
+// MiddleWare
+// to get request body parsed
+app.use(express.json());
+
+// get all todos
 app.get('/api/todos', (req, res) => {
   res.json(todoDb);
 });
-
+// get one todo
 app.get('/api/todos/:id', (req, res) => {
   const found = findTodoWithId(req.params.id);
-  console.log(found);
-  res.json({ rez: found });
+  // console.log(found);
+  if (!found) {
+    handleFindErr(req.params.id, res);
+    return;
+  }
+  res.json(found);
+});
+
+// delete one todo
+app.delete('/api/todos/:id', (req, res) => {
+  const paramId = req.params.id;
+  const found = findTodoWithId(paramId);
+
+  if (!found) {
+    handleFindErr(paramId, res);
+    return;
+  }
+
+  handleDelete(found);
+
+  console.log(`deleted ${found.title}`);
+  res.json({ deleted: found, todoDb });
+});
+
+app.post('/api/todos/', (req, res) => {
+  console.log(`server got body ${req.body}`);
+
+  const { title } = req.body;
+
+  handleAdd(title);
+
+  res.json({ msg: 'success', todoDb });
+});
+
+app.put('/api/todos/:id', (req, res) => {
+  const paramId = req.params.id;
+  const found = findTodoWithId(paramId);
+
+  if (!found) {
+    handleFindErr(paramId, res);
+    return;
+  }
+
+  const { id, title, done } = req.body;
+
+  found.id = id;
+  found.title = title;
+  found.done = done;
+
+  res.json({ edited: found, todoDb });
 });
 
 app.listen(3000, () => console.log('server os running'));
